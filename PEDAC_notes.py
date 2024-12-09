5836dee3780d4534d845496f13edeafbf671ab45
vd141 <victor.dinh141@gmail.com>
2024-12-08 22:06:26 -0800

    PEDAC notes init commit

---
+++ PEDAC_notes.py
@@ -0,0 +1,13 @@
+'''
+P - understand the Problem
+E - Examples / Test Cases
+D - Data Structures
+A - Algorithms
+C - Code
+
+
+1. write down inputs
+2. write down outputs
+3. write down explicit requirements
+4. write down implicit requirements
+'''
