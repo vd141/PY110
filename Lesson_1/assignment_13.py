89e8e7b7543c27760f6d0a15103a01b4027f3ba1
vd141 <victor.dinh141@gmail.com>
2024-12-13 14:24:54 -0800

    completed assignment 13

--- Lesson_1/assignment_13.py
+++ Lesson_1/assignment_13.py
@@ -17,3 +17,47 @@ def select_fruit(produce):
 
 select_fruit(produce)
 print(select_fruit(produce))  # { apple: 'Fruit', pear: 'Fruit' }
+
+def double_numbers(numbers):
+    for index, _ in enumerate(numbers):
+        numbers[index] *= 2
+    return numbers
+
+'''
+suppose we wanted to transform the numbers based on their position in the list rather
+than their value? Try coding a solution that doubles the numbers that have odd indexes
+'''
+
+def double_odd_indexes(numbers):
+    '''
+    input: list of numbers
+    output: list of numbers whose odd indexes are doubled versions of the input list
+    '''
+
+    doubled_odd_indexes = []
+
+    for index, value in enumerate(numbers):
+        if index % 2 == 1:
+            doubled = numbers[index] * 2
+            doubled_odd_indexes.append(doubled)
+        else:
+            same_value = numbers[index]
+            doubled_odd_indexes.append(same_value)
+    
+    return doubled_odd_indexes
+
+my_numbers = [1, 4, 3, 7, 2, 6]
+print(double_odd_indexes(my_numbers)) # 1, 8, 3, 14, 2, 12
+
+
+def multiply(numbers, factor):
+    multiplied = []
+
+    for number in numbers:
+        multiplied.append(number * factor)
+
+    return multiplied
+
+
+my_numbers = [1, 4, 3, 7, 2, 6]
+print(multiply(my_numbers, 3))  # [3, 12, 9, 21, 6, 18]
