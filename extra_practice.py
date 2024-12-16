a1cbf823745e5453c82f40a5d18759d00adda28c
vd141 <victor.dinh141@gmail.com>
2024-12-16 13:02:04 -0800

    added questions from practice problems lesson 2

--- extra_practice.py
+++ extra_practice.py
@@ -231,3 +231,14 @@ What does the following code print and why?
 '''
 dictionary = {'a': 'ant', 'b': 'bear'}
 print(dictionary.popitem())
+
+'''
+return a new list identical in structure to the original but with each number incremented
+by 1. do not modify the original data structure. use a comprehension
+'''
+
+lst = [{'a': 1}, {'b': 2, 'c': 3}, {'d': 4, 'e': 5, 'f': 6}]
+
+new_dict = [dict(zip(subdict.keys(), [sub_values + 1 for sub_values in subdict.values()])) for subdict in lst]
+print(new_dict)
+print(new_dict == [{'a': 2}, {'b': 3, 'c': 4}, {'d': 5, 'e': 6, 'f': 7}])
