21d77747391ac2d0cd1df82421d462fdbdbcd6ab
vd141 <victor.dinh141@gmail.com>
2025-01-23 09:46:31 -0800

    renamed and added notes

--- PEDAC_session_01212025.py
+++ PEDAC_session_01212025.py
@@ -1,3 +1,45 @@
+'''
+PEDAC Notes
+"""
+P - Understanding the Problem:
+- Goal: understand what you're being asked to do
+  - Read the problem description
+  - Identify expected input and output
+  - Establish rules/requirements/define the boundaries of the problem
+  - Ask clarifying questions
+  - Take your time on this step!
+
+E - Examples and Test Cases:
+- Goal: further expand and clarify understanding about what you're being asked to do 
+  - Use examples/test cases to confirm or refute assumptions made about the problem in the previous step
+
+D - Data Structures:
+- Goal: begin to think logically about the problem
+  - What data structures could we use to solve this problem?
+    - What does our data look like when we get it? (input)
+    - What does our data look like when we return it? (output?)
+    - What does our data need to look like in the intermediate steps?
+
+A - Algorithm:
+- Goal: write out steps to solve the given problem in plain English
+  - A logical sequence of steps for accomplishing a task/objective
+  - Start high level, but make sure you've thought through and have provided sufficient detail for working through the most difficult parts of the problem
+  - Stay programming-language-agnostic
+  - Can create substeps for different parts or separate concerns into a helper method
+  - You can (and should) revisit your algorithm during the implementation stage if you need to refine your logic
+  - Before moving onto implementing the algorithm, check it against a few test cases
+
+C - Implementing a Solution in Code:
+- Goal: translate the algorithm into your programming language of choice
+  - Code with intent. Avoid hack and slash 
+- TEST FREQUENTLY
+  - Use the REPL or run your code as a file
+  - When testing your code, always have an idea in place of what you're expecting
+  - If you find that your algorithm doesn't work, return there FIRST if needed and THEN fix your code
+"""
+'''
+
+
 """
 We're receiving a set of messages in code. The messages are sets of text strings, like:
 "alakwnwenvocxzZjsf"
