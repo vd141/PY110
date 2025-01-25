ab6e24994c69cf1cc563d57aba028b36196cbad8
vd141 <victor.dinh141@gmail.com>
2025-01-24 16:10:03 -0800

    completed

--- Exercises/Medium1/Second_Pass/fibonacci_numbers_recursion.py
+++ Exercises/Medium1/Second_Pass/fibonacci_numbers_recursion.py
@@ -1,4 +1,23 @@
-def sum_recursive(n):
+def fibonacci(n):
     '''
     fibonacci sequence, recursively
+
+    base case: if n is 1 or 2, return 1
     '''
+
+    if n < 1:
+        return
+    
+    if n in [1, 2]:
+        return 1
+    
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(1) == 1)         # True
+print(fibonacci(2) == 1)         # True
+print(fibonacci(3) == 2)         # True
+print(fibonacci(4) == 3)         # True
+print(fibonacci(5) == 5)         # True
+print(fibonacci(6) == 8)         # True
+print(fibonacci(12) == 144)      # True
+print(fibonacci(20) == 6765)     # True
